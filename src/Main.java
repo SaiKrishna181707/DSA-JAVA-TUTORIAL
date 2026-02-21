47b0f96e84d4778906002ac921973b142ae33f23
kittu <kittusontineni@gmail.com>
2026-02-21 08:43:18 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main() {
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Hello");
+//        System.out.println("world");
+//        System.out.print("hello");
+//        System.out.print("world");
+//        System.out.printf("Age is %d", 20);
+        int num = input.nextInt();
+        System.out.println(input.nextLine());
+
     }
 }
