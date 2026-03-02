d36e9dff1daeeaf4b32c7f1e23e1c0ef5c78befb
kittu <kittusontineni@gmail.com>
2026-03-02 16:57:24 +0530

    feat:implemented patterns using loops

---
+++ src/Patterns.java
@@ -0,0 +1,14 @@
+public class Patterns {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+    static void pattern(int n){
+        for (int row = 1; row <=4 ; row++) {
+            for (int col = 1; col <=row ; col++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+}
