26a4488d6d84196fc1e2a7dbd8d4290823e107d3
kittu <kittusontineni@gmail.com>
2026-03-13 17:09:45 +0530

    fix: Updated Logic Bug.

--- src/DigitProduct.java
+++ src/DigitProduct.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-
-public class DigitSum {
+public class ProductSum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int result = sum(n);
+        int result = product(n);
         System.out.println(result);
     }
-    public static int sum(int n) {
-        if (n==0) return 0;
-        return (n%10) + sum(n/10);
+    public static int product(int n) {
+        if (n==1) return 1;
+        return (n%10) * product(n/10);
     }
 }
