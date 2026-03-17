6d556257be550352c86f3e593eb49146e57cc748
kittu <kittusontineni@gmail.com>
2026-03-17 17:15:18 +0530

    feat: implemented selection sort using recursion

--- src/SelectionSortRecursion.java
+++ src/SelectionSortRecursion.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class SelectionSortRecursion {
+    public static void main(String[] args) {
+        int[] arr = {2,7,1,6};
+        int size = arr.length;
+        Ssr(arr,size,0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void Ssr(int[] arr, int r , int c , int max) {
+        if (r==0) return;
+        if (c<r) {
+            if(arr[c] > arr[max]){
+                Ssr(arr,r,c+1,c);
+            }
+            else{
+                Ssr(arr,r,c+1,max);
+            }
+        }else{
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            Ssr(arr,r-1,0,0);
+        }
+
+    }
 }
