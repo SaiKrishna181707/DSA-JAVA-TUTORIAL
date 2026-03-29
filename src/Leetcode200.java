c84a1fc04e1f93485e6c9b384928e2e516c12930
Kittu <kittusontineni@gmail.com>
2026-03-29 09:07:42 +0530

    feat: Finding number of islands using backtracking solution.

---
+++ src/Leetcode200.java
@@ -0,0 +1,46 @@
+public class Leetcode200 {
+    public static void main(String[] args) {
+
+        char[][] grid = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+
+        int answer = numIslands(grid);
+        System.out.println(answer);
+    }
+
+    public static int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int r, int c) {
+
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length ||
+                grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
